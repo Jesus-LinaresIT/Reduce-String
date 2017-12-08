07125ee153a595a1af0f2c2cb775a1439bde476f
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-08 14:44:55 -0600

    Super Reduce String

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace ProgramList
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            String cadena = "";
+            Console.WriteLine("Escribe la cadena de texto: ");
+            cadena = Console.ReadLine();
+            var letras = cadena.ToCharArray();
+            string letters = new string(letras);
+            Console.WriteLine("Cadena original: {0}", cadena);
+            Console.WriteLine("Iteracion realizada: ");
+            List<char> evaluacion = new List<char>();
+            bool bandera = false;
+            string datoR = "";
+
+            for (int c = 0; c < letras.Length; c++)
+            {
+
+                Console.WriteLine("{1}", c, cadena[c]);
+
+                if (c == 0)
+                {
+                    evaluacion.Add(letras[c]);
+                }
+                else
+                {
+
+                    foreach (char dr in evaluacion)
+                    {
+                        if (dr == letras[c])
+                        {
+
+                            bandera = true;
+                            continue;
+
+                        }
+                    }
+
+
+                    if (bandera == false)
+                    {
+
+                        evaluacion.Add(letras[c]);
+                    }
+                    else {
+
+                        bandera = false;
+
+                    }
+
+
+                }
+            
+            }
+
+            foreach (char item in evaluacion)
+            {
+                
+                datoR = datoR + item;                
+                
+            }
+
+
+            Console.WriteLine("n/Cadena final: ");
+            Console.WriteLine(datoR);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
